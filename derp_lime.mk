ae9cb630a18c71551c25e43b8ef119c67f76c617
NurKeinNeid <mralexman3000@gmail.com>
2021-04-02 23:00:51 +0200

    chime: DerpFest!
    Change-Id: Ie0257a68f6a0140b98fb7d104dc2ffd1f5c2afde
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>
    Signed-off-by: c3eru <yyprjkt@gmail.com>

--- derp_lime.mk
+++ derp_lime.mk
@@ -7,14 +7,14 @@
 # Inherit from lime device
 $(call inherit-product, device/xiaomi/chime/device-lime.mk)
 
-# Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-TARGET_USES_AOSP_RECOVERY := true
+# Inherit some common Derpfest stuff stuff.
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
+USE_LEGACY_BOOTANIMATION := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_lime
+PRODUCT_NAME := derp_lime
 PRODUCT_DEVICE := lime
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 9T
